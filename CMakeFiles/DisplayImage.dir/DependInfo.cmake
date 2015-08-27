f882fa9eb6952fe2c9221c440d452ec773db1e0c
K. J. Nitthilan <nitthilan@gmail.com>
2015-08-27 11:50:05 +0530

    Setting up opencv c++ application

---
+++ CMakeFiles/DisplayImage.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/workspace/opencv_c++_test/DisplayImage.cpp" "/Users/nitthilan/Documents/myfolder/project_devan/opencv/workspace/opencv_c++_test/CMakeFiles/DisplayImage.dir/DisplayImage.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/build"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/include/opencv"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/hal/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/core/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/flann/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/imgproc/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/ml/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/photo/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/video/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/imgcodecs/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/shape/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/videoio/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/highgui/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/objdetect/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/superres/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/ts/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/features2d/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/calib3d/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/stitching/include"
+  "/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/modules/videostab/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ readme.md
@@ -0,0 +1,20 @@
+# For making opencv3 installed in a different directory we should do the following:
+- Based on http://www.learnopencv.com/install-opencv-3-on-yosemite-osx-10-10-x/, we have to copy the package file to /usr/local
+- cp lib/pkgconfig/opencv.pc /usr/local/lib/pkgconfig/opencv3.pc
+- now the package name is not opencv but opencv3
+	- open the opencv3.pc stores the path for libs and includes requires by the package
+- Set the path for opencv using OpenCV_DIR env variable
+	- export OpenCV_DIR=/Users/nitthilan/Documents/myfolder/project_devan/opencv/installation/opencv/build/
+	- Adding this to the .profile file
+- Running cmake creates a CMakeCache.txt file. To make sure cmake runs properly delete it before running again
+- Do not try using FindOpenCV.cmake file since modifying opencv package name to opencv3 did not work. So probably would work with direct installation
+- message(STATUS ${OpenCV_LIBS}) in CMakeLists.txt would help in dumping the variable during cmake
+- to check whether the build is working without cmake
+	- g++ -ggdb `pkg-config --cflags --libs opencv3` facedetect.cpp -o /tmp/test && /tmp/test
+- check the output of pkg-config value to check the libraries and include values for the package
+	-  pkg-config --cflags --libs opencv3
+
+# Steps to build:
+- cmake .
+- make
+- 
